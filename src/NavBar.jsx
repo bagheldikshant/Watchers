6e7e3de3ebda4b0670924376add9f8d0ceddb65e
bagheldikshant <94803450+bagheldikshant@users.noreply.github.com>
2022-03-29 14:25:25 +0530

    bug-fix

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,34 +3,34 @@ class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav class="navbar navbar-light bg-light">
-          <a class="navbar-brand " href="/">
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand " href="/">
             <img
-              className="logo"
+              classNameName="logo"
               src="https://e7.pngegg.com/pngimages/996/491/png-clipart-shopify-e-commerce-logo-web-design-design-web-design-logo.png"
               width="30"
               height="30"
-              alt=""
+              alt="dick"
             />
             <span className="logo-text">Watchers.com</span>
           </a>
-          <div class="order-lg-last col-lg-5 col-sm-8 col-8">
-            <div class="float-end">
-              <a href="/" class="btn btn-light nav-button btn-sm nav-button">
-                <i class="fa fa-user"></i>
-                <span class="ms-1 d-none d-sm-inline-block ">Sign in </span>
+          <div className="order-lg-last col-lg-5 col-sm-8 col-8">
+            <div className="float-end">
+              <a href="/" className="btn btn-light nav-button btn-sm nav-button">
+                <i className="fa fa-user"></i>
+                <span className="ms-1 d-none d-sm-inline-block ">Sign in </span>
               </a>
-              <a href="/" class="btn btn-light btn-sm nav-button">
-                <i class="fa fa-heart"></i>
-                <span class="ms-1 d-none d-sm-inline-block ">Wishlist</span>
+              <a href="/" className="btn btn-light btn-sm nav-button">
+                <i className="fa fa-heart"></i>
+                <span className="ms-1 d-none d-sm-inline-block ">Wishlist</span>
               </a>
               <a
                 data-bs-toggle="offcanvas"
-                href="#offcanvas_cart"
-                class="btn btn-light btn-sm nav-button"
+                href="/"
+                className="btn btn-light btn-sm nav-button"
               >
-                <i class="fa fa-shopping-cart"></i>
-                <span class="ms-1">My cart </span>
+                <i className="fa fa-shopping-cart"></i>
+                <span className="ms-1">My cart </span>
               </a>
             </div>
           </div>
--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -4,51 +4,51 @@ class SideBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <div class="container-fluid">
-          <div class="row">
+        <div className="container-fluid">
+          <div className="row">
             <div
-              class="col-2 collapse show d-md-flex bg-light pt-2 pl-0 min-vh-100"
+              className="col-2 collapse show d-md-flex bg-light pt-2 pl-0 min-vh-100"
               id="sidebar"
             >
-              <ul class="nav flex-column flex-nowrap overflow-hidden">
-                <li class="nav-item sidebar-item">
-                  <a class="nav-link text-truncate" href="/">
-                    <span class="d-none d-sm-inline">Account Main</span>
+              <ul className="nav flex-column flex-nowrap overflow-hidden">
+                <li className="nav-item sidebar-item">
+                  <a className="nav-link text-truncate" href="/">
+                    <span className="d-none d-sm-inline">Account Main</span>
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-truncate" href="/">
-                    <span class="d-none d-sm-inline">New Orders</span>
+                <li className="nav-item">
+                  <a className="nav-link text-truncate" href="/">
+                    <span className="d-none d-sm-inline">New Orders</span>
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-truncate" href="/">
-                    <span class="d-none d-sm-inline">Orders History</span>
+                <li className="nav-item">
+                  <a className="nav-link text-truncate" href="/">
+                    <span className="d-none d-sm-inline">Orders History</span>
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-truncate" href="/">
-                    <span class="d-none d-sm-inline">My Wishlist</span>
+                <li className="nav-item">
+                  <a className="nav-link text-truncate" href="/">
+                    <span className="d-none d-sm-inline">My Wishlist</span>
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-truncate" href="/">
-                    <span class="d-none d-sm-inline">Transactions</span>
+                <li className="nav-item">
+                  <a className="nav-link text-truncate" href="/">
+                    <span className="d-none d-sm-inline">Transactions</span>
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-truncate" href="/">
-                    <span class="d-none d-sm-inline">Profile Setting</span>
+                <li className="nav-item">
+                  <a className="nav-link text-truncate" href="/">
+                    <span className="d-none d-sm-inline">Profile Setting</span>
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link text-truncate" href="/">
-                    <span class="d-none d-sm-inline">Log out</span>
+                <li className="nav-item">
+                  <a className="nav-link text-truncate" href="/">
+                    <span className="d-none d-sm-inline">Log out</span>
                   </a>
                 </li>
               </ul>
             </div>
-            <div class="col pt-2">
+            <div className="col pt-2">
               <MainPage />
             </div>
           </div>
